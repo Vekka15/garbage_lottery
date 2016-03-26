cab0bb1a6af808ccb776f8d30373ec838985d818
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-26 17:48:16 +0100

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 # Load the app's custom environment variables
-hidden_variables = File.join(Rails.root, 'config', 'initializers', 'hidden_variables.rb')
-load(hidden_variables) if File.exists?(hidden_variables)
+# hidden_variables = File.join(Rails.root, 'config', 'initializers', 'hidden_variables.rb')
+# load(hidden_variables) if File.exists?(hidden_variables)
 
 # Initialize the Rails application.
 Rails.application.initialize!
