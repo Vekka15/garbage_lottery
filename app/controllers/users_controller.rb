7bea633b5cbe8c1027bfee241cb59e7d010c1448
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-31 00:45:50 +0200

    proper time zone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,6 @@ class UsersController < ApplicationController
       if !Invitation.where(email: @new_user.email).empty?
         @new_user.invited = true
         Invitation.where(email: @new_user.email).first.delete
-        params[:invitation_id]=@new_user.email
       end
       if @new_user.save
         InvitationMailer.send_notification_add(@new_user).deliver_now
