14e3452ef714098257438b72ae47c5cf1c5b233b
Zuzanna Sawala <zuzanna.sawala@student.put.poznan.pl>
2016-03-31 02:22:12 +0200

    end

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,6 +2,6 @@ class Invitation < ActiveRecord::Base
   validates :email,
     presence: { message: 'Email is required' },
     uniqueness: { message: 'Invitation with this email already exist' },
-    format: { with: /\w\@prograils.com/, message: 'Not proper format', allow_blank: true },
+    format: { with: /\w\@prograils.com/, message: 'should have .prograils.com', allow_blank: true },
     email: true
 end
